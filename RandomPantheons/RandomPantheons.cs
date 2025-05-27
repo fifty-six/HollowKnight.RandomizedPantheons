b6c5b00467e52cb20c95254d9a1765000792ef69
Yusuf Bham <git@fiftysix.dev>
2025-05-27 01:50:46 -0400

    Fix the HUD directly based on the previous scene
    Bumps the version to 1.3.0.0. Closes #5.

--- RandomPantheons/RandomPantheons.cs
+++ RandomPantheons/RandomPantheons.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Modding;
 using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
 using Random = System.Random;
 using USceneManager = UnityEngine.SceneManagement.SceneManager;
 using Vasi;
@@ -31,25 +33,9 @@ namespace RandomPantheons
             // Causes an infinite loop
             "GG_Spa"
         ];
-
-        // Some scenes cause HUD to disappear
+        
         private static readonly List<string> VanishedHUD =
         [
-            "GG_Hollow_Knight",
-            "GG_Radiance"
-        ];
-
-        // Some scenes let HUD to re-appear
-        private static readonly List<string> AppearedHUD =
-        [
-            "GG_Unn",
-            "GG_Wyrm",
-            "GG_Engine",
-            "GG_Engine_Prime",
-            "GG_Engine_Root",
-            "GG_Spa",
-            "GG_Grimm",
-            "GG_Grimm_Nightmare",
             "GG_Hollow_Knight",
             "GG_Radiance"
         ];
@@ -68,6 +54,30 @@ namespace RandomPantheons
             On.BossSequenceDoor.Start += RandomizeSequence;
             // Fixes the door for P5
             On.PlayMakerFSM.Start += ModifyRadiance;
+
+            USceneManager.activeSceneChanged += OnSceneChange;
+        }
+
+        private void OnSceneChange(Scene prev, Scene next)
+        {
+            if (GameManager.instance == null)
+                return;
+
+            IEnumerator CheckHud()
+            {
+                yield return null;
+                
+                // :CAUGHT:
+                if (GameCameras.instance)
+                {
+                    GameCameras.instance.hudCanvas.LocateMyFSM("Slide Out").SendEvent("IN");
+                }
+            }
+
+            if (VanishedHUD.Contains(prev.name))
+            {
+                GameManager.instance.StartCoroutine(CheckHud());
+            }
         }
 
         private static void ModifyRadiance(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self)
@@ -102,13 +112,8 @@ namespace RandomPantheons
                 }
 
                 // Check the conditions
-                bool f = InvalidFirst.Contains(scenes[0].sceneName) ||
-                         InvalidLast.Contains(scenes[scenes.Count - 1].sceneName);
-                
-                for (int i = 1; i < scenes.Count; i++)
-                    if (scenes[i - 1].sceneName == scenes[i].sceneName ||
-                        VanishedHUD.Contains(scenes[i - 1].sceneName) && !AppearedHUD.Contains(scenes[i].sceneName))
-                        f = true;
+                bool f = InvalidFirst.Contains(scenes[0].sceneName) 
+                      || InvalidLast.Contains(scenes[scenes.Count - 1].sceneName);
                 
                 if (!f)
                     break;
